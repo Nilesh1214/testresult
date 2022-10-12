2057f1d1de8ab91402912392d8c329e178b1848c
Lenovo <Lenovo@DESKTOP-VCH08BI>
2022-10-12 09:56:33 +0530

    deta type

---
+++ testresult/src/testresult/simple.java
@@ -0,0 +1,54 @@
+package testresult;
+
+public class simple {
+
+	public static void main(String[] args) {
+		System.out.println("hello team...! welcome to java session") ; {
+			String student = "all good";
+			System.out.println("let start");
+
+			byte b = (byte)130;
+			System.out.println(b);
+
+			byte B2 = -128;
+			System.out.println(B2);
+
+			byte A1=(byte)600;
+			System.out.println(A1);
+
+			byte A2 = 88;
+			System.out.println(A2);
+
+			short A3 = 90;
+			System.out.println(A3);
+
+			int A5=128;
+			System.out.println(A5);
+
+			long A6= 123_123_123;
+			System.out.println(A6);
+			
+			float h9 = 2.6f*4;
+			long Gh =(long)h9;
+			System.out.println(Gh);
+			
+			int Q1= 070;
+					System.out.println(Q1);
+					
+					int Q2= 0X25;
+					System.out.println(Q2);
+					
+					float H1 = 2.12F;
+					System.out.println(H1);
+					
+					double Q3 = 12.2D;;
+					System.out.println(Q3);
+					
+					boolean H2 = false;
+					System.out.println(H2);
+
+
+		}
+
+	}
+}
