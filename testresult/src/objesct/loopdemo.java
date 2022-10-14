99eede91e94c7243454c4110a5f1a82654d983dd
Lenovo <Lenovo@DESKTOP-VCH08BI>
2022-10-14 10:58:03 +0530

    Loops

---
+++ testresult/src/objesct/loopdemo.java
@@ -0,0 +1,23 @@
+package objesct;
+
+public class loopdemo {
+
+	public static void main(String[] args) {
+		// 3 loop\ while , do while , for
+		
+		int i = 0;
+		while (i<10) {
+			System.out.println(i);
+		    i++;
+		 
+		}
+          int p =20;
+        	do {
+        		System.out.println(p);
+        		p++;
+        	}while (p>20); 
+        		
+        	}
+	}
+
+
