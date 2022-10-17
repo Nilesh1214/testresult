ff74ef0ed23e404b0c5e82264d03c1bf2028ea56
Lenovo <Lenovo@DESKTOP-VCH08BI>
2022-10-17 10:10:28 +0530

    All Operater with for

--- testresult/src/objesct/loopdemo.java
+++ testresult/src/objesct/loopdemo.java
@@ -9,17 +9,37 @@ public class loopdemo {
 		while (i<10) {
 			System.out.println(i);
 		    i++;
-		 
 		}
-          int p =20;
-        	do {
-        		System.out.println(p);
-        		p++;
-        	}while (p<20); 
-	}}
+	
+    int p =20;
+  	do {
+  		System.out.println("nilesh");
+  		p++;
+  	}while (p<20); 
+  	
+  	for (int M=0;M<20;M++) {
+  	System.out.println("hello i am Nilesh ");
+	
+  	}
+  
+  	int P=0;
+  			while (P<20) {
+  				System.out.println(P);
+  				P++;
+  			}
+  			
+  			int Z=0;
+  			do {
+  			System.out.println("Hell team");
+  			Z++;
+	         }while (Z<30);
+  			
+  		
+  			for(int Q=1;Q<=10;Q++) {
+  				System.out.println("nilu");
+  			}
 
+	}
+}
 	
 	
-
-
-
